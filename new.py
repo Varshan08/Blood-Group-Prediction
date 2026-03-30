a3483bdc870c351660b689b0920c027a2c85bccc
Varshan08 <varshanyuvaraj08@gmail.com>
2026-03-30 15:40:50 +0530

    added

--- new.py
+++ new.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
-# load old model
 model = tf.keras.models.load_model("cnn_model.h5")
 
-# re-save in compatible format
-model.save("cnn_model_fixed.h5", save_format="h5")
+# THIS is the correct way for Keras 3
+model.export("cnn_model_tf")
